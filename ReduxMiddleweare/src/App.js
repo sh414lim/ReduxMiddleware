21d8f7721188474674921cec8e3735abe2582882
DESKTOP-1HI9LQN\sh414 <sh414lim@Gmail.com>
2021-04-06 16:33:50 +0900

    Counter Middleweare

---
+++ ReduxMiddleweare/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CounterContainer from './containers/CounterContainers';
+
+
+function App() {
+
+  return(
+    <div>
+      <CounterContainer/>
+    </div>
+
+  )
+
+}
+
+export default App;
